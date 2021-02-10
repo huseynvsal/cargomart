e042234618ddd70e4759877f5c8c4c87f92d8ded
Vüsal <huseynlivusal25@gmail.com>
2021-02-10 17:57:00 +0400

    123

--- public/js/script.js
+++ public/js/script.js
@@ -32,23 +32,6 @@ if($('.tabs-box').length){
 }
 
 
-if($('.tabs-box').length){
-  $('.tabs-box .tab-buttons .tab-btn').on('click', function(e) {
-    e.preventDefault();
-    var target = $($(this).attr('data-tab'));
-    
-    if ($(target).is(':visible')){
-      return false;
-    }else{
-      target.parents('.tabs-box').find('.tab-buttons').find('.tab-btn').removeClass('active-btn');
-      $(this).addClass('active-btn');
-      target.parents('.tabs-box').find('.tabs-content').find('.tab').fadeOut(0);
-      target.parents('.tabs-box').find('.tabs-content').find('.tab').removeClass('active-tab');
-      $(target).fadeIn(300);
-      $(target).addClass('active-tab');
-    }
-  });
-}
 
 $(document).ready(function(){
     $('.customer-logos').slick({
@@ -141,3 +124,12 @@ $(document).ready(function(){
 			
 		});	
 	} 
+
+ 
+
+  $('.nav-tabs-dropdown')
+    .on("click", "li:not('.active') a", function(event) {  $(this).closest('ul').removeClass("open");
+    })
+    .on("click", "li.active a", function(event) {        $(this).closest('ul').toggleClass("open");
+    });
+
--- resources/views/main/package.blade.php
+++ resources/views/main/package.blade.php
@@ -27,7 +27,7 @@
                     <ul class="list clearfix">
 
                         <li>
-                            <a href="beyan.html"><i class="fas fa-plus"></i>Bəyan et</a>
+                            <a href="/order"><i class="fas fa-plus"></i>Bəyan et</a>
                         </li>
                         <li>
                             <a href="/package"><i class="fas fa-calendar-alt"></i>Bağlamalar</a>
@@ -53,42 +53,111 @@
                             <!-- dashboard link slider -->
                             <div class="tabs-box">
                                 <div class="tab-btn-box centred">
-                                    <ul class="tab-btns tab-buttons clearfix">
-                                        <li class="active-btn">
-                                            <a href="/package">
-                                                Öncədən bəyan et
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="/package">
-                                                Xarİcİ anbarda
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="/package">
-                                                GÖndƏrİlİb
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="/package">
-                                                Bakıda
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="/package">
-                                                Keçmİş sİfarİşlər
-                                            </a>
-                                        </li>
+                                    <ul class="nav nav-tabs nav-justified nav-tabs-dropdown tab-btns tab-buttons clearfix" role="tablist">
+                                    <li class="col-lg-3" role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Öncədən bəyan et</a></li>
+                                    <li class="col-lg-2" role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Xarici anbarda</a></li>
+                                    <li class="col-lg-2" role="presentation"><a href="#messages" aria-controls="messages" role="tab" data-toggle="tab">Göndərilib</a></li>
+                                    <li class="col-lg-2" role="presentation"><a href="#settings" aria-controls="settings" role="tab" data-toggle="tab">Bakıda</a></li>
+                                    <li class="col-lg-3" role="presentation"><a href="#settings" aria-controls="past" role="tab" data-toggle="tab">Keçmiş sifarişlər</a></li>
                                     </ul>
                                 </div>
                                 <div class="tabs-content">
-                                    <div class="tab active-tab" id="tab-1">
+                                    <div class="tab active-tab">
                                         <div class="inner-box">
-                                            <div class="accordion-box">
+                                            
+                                            <div class="tab-content">
+                                                <div role="tabpanel" class="tab-pane active" id="home">
+                                                    <div class="accordion-box">
+                                                        <div class="alert alert-warning" role="alert">Heç bir bağlamanız yoxdur.</div>
+                                                    </div>
+                                                </div>
+                                                <div role="tabpanel" class="tab-pane" id="profile">
+                                                    <div class="accordion-box">
+                                                                                                                                                                                                                                                            <div class="alert alert-info" role="alert">Çəki və Çatdırılma qiyməti dəyişə bilər.</div>
+                                                                                                    
+                                                                                                    <div class="title-box">
+                                                        <h6>Bağlamalar</h6>
+                                                    </div>
+                                                    <ul class="accordion-inner">
+                                                                                                                    <li class="accordion block">
+                                                                <div class="acc-btn">
+                                                                    <div class="icon-outer"></div>
+                                                                    <h6> fontawesome.com (Türkiyə) -  № MBX567377136</h6>
+                                                                </div>
+                                                                <div class="acc-content">
+                                                                    <div class="accordion__text-block" style="display: block;">
+                                                                        <ul class="list list--check list--reset">
 
-                                                                                                                                                                    <div class="alert alert-warning" role="alert">Heç bir bağlamanız yoxdur.</div>
-                                                                                                                                                    </div>
-                                        </div>
+                                                                            <li class="list__item"><strong>
+                                                                                    Bağlama tarixi:
+                                                                                </strong> 10.02.21
+                                                                            </li>
+                                                                            <li class="list__item">
+                                                                                <strong>Trek nömrə</strong>
+                                                                                MBX567377136
+                                                                            </li>
+                                                                          
+                                                                            <li class="list__item"><strong>
+                                                                                    Mağaza:
+                                                                                </strong>
+                                                                                fontawesome.com
+                                                                            </li>
+                                                                            <li class="list__item">
+                                                                                <strong>Məhsul sayı</strong>
+                                                                                56
+                                                                            </li>
+                                                                            <li class="list__item">
+                                                                                <strong>Çəki:</strong>
+                                                                                0 kg
+                                                                            </li>
+                                                                                                                                                            <li class="list__item">
+                                                                                    <strong>
+                                                                                        Dəyəri:
+                                                                                    </strong>
+                                                                                    890 TRY
+                                                                                </li>
+                                                                                                                                                        <li class="list__item">
+                                                                                <strong>
+                                                                                    Çatdırılma qiyməti
+                                                                                </strong>
+                                                                                -
+                                                                            </li>
+                                                                           
+
+                                                                            <li class="list__item">
+                                                                                <strong>
+                                                                                    Bəyannamə:
+                                                                                </strong>
+                                                                                <a target="_blank" href="https://mobex.az/uploads/packages/6023e541ca3cc.jpg">invoys</a>
+                                                                            </li>
+                                                                                                                                                            <li class="list__item">
+                                                                                    <strong>
+                                                                                        Qeyd
+                                                                                    </strong>
+                                                                                    65126
+                                                                                </li>
+                                                                                                                                                    </ul>
+                                                                        <div class="full accordion_inbutton_cont">
+
+                                                                                                                                                            <div class="col-lg-3 col-md-4 col-sm-12 form-group message-btn accordion_inbutton">
+                                                                                    <a href="https://mobex.az/user/declaration/7627" class="theme-btn-one"> Dəyiş<i class="icon-Arrow-Right"></i></a>
+                                                                                </div>
+                                                                                                                                                                    <div class="col-lg-3 col-md-4 col-sm-12 form-group message-btn accordion_inbutton">
+                                                                                        <a href="https://mobex.az/user/declaration/delete/7627" class="theme-btn-one" style="background-color: #ff1c90"> Sil<i class="icon-Arrow-down-4"></i></a>
+                                                                                    </div>
+                                                                                                                                                                                                                                        
+                                                                        </div>
+                                                                    </div>
+                                                                </div>
+                                                            </li>
+                                                                                                            </ul>
+                                                                                            </div>
+                                                </div>
+                                                <div role="tabpanel" class="tab-pane" id="messages">Messages...</div>
+                                                <div role="tabpanel" class="tab-pane" id="settings">Settings...</div>
+                                                <div role="tabpanel" class="tab-pane" id="past">Settings...</div>
+                                            </div>
+                                       
 
                                     </div>
 
